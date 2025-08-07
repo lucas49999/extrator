4efaa558816f73934a6f5670671ef6b40d0e281d
lucas49999 <lucas.soares09@escola.pr.gov.br>
2025-08-07 11:48:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChaves);
+function mostraPalavrasChaves(){
+    alert('fui clicado!');
+}
